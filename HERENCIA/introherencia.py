afde247e0f24fb1f6930f4d79bfa666f5441533e
vasquezsharona <116228242+vasquezsharona@users.noreply.github.com>
2023-03-06 13:22:51 -0500

    Create introherencia.py

---
+++ HERENCIA/introherencia.py
@@ -0,0 +1,7 @@
+def  suma ( a , b ): # se crea una funcion nombrada (suma) asignandole dos parametros ayb
+    return  a + b  # devuelve el valor de (a) mas (b)
+
+def  suma ( a , b , c ): # sse crea otra funcion nombrada (suma) asignandole tres parametros a,byc
+    return  a + b + c  # devuelve el valor de (a) mas (b) mas (c)
+
+print ( suma ( 1 , 2 , 3 )) # imprima, la suma de los parametros asignados que son 1 mas 2 mas 3
