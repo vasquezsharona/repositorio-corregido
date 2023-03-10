6e7e28d5aac457c2b4a3383ada2501bc2c65be6b
vasquezsharona <116228242+vasquezsharona@users.noreply.github.com>
2023-03-10 17:29:13 -0500

    Create conductor.py

---
+++ POO-ARCHIVO/conductor.py
@@ -0,0 +1,8 @@
+ conductor de clase : #se crea una clase
+    def  __init__ ( self , nombre , documento ): # se inicializa un constructor con parámetros asignados
+        uno mismo __nombre = nombre  # se le asigna el atributo al parametro
+        uno mismo __documento = documento  # se le asigna el atributo al parametro
+    def  getNombre ( self ): # se crea una clase
+        devolverse  a uno mismo . __nombre  # devuelve el valor del atributo
+    def  getDocumento ( self ): # se crea una clase
+        devolverse  a uno mismo . __documento  # devuelve el valor del atributo
