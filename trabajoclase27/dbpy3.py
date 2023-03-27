031a17b9f0cc5995cd221301bbef5fb12e5f9a93
vasquezsharona <116228242+vasquezsharona@users.noreply.github.com>
2023-03-27 14:42:41 -0500

    Create dbpy3.py

---
+++ trabajoclase27/dbpy3.py
@@ -0,0 +1,5 @@
+importar  sqlite3
+contra = sqlite3 . conectar ( 'C: \\ narváez \\ db \\ conpython.db' )
+imprimir ( escribir ( con ))
+micursor = estafa . cursor ()
+imprimir ( escribir ( micursor ))
